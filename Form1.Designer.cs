d2b9143c7f3ef1364b6fec350ab7f34690813627
Muhammed Osman Dualı <osmandvali@gmail.com>
2023-02-06 13:44:54 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace taskagıtmakas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(52, 226);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(328, 36);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Location = new System.Drawing.Point(52, 268);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Taş";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button2.Location = new System.Drawing.Point(166, 268);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 52);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Kağıt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.Control;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button3.Location = new System.Drawing.Point(276, 268);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 52);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Makas";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(444, 184);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bot : 0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(413, 226);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Kullanıcı : 0";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox4.Location = new System.Drawing.Point(52, 157);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(328, 36);
+            this.textBox4.TabIndex = 11;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(15, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(481, 51);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Taş Kağıt Makas Oyunu";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 338);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(15);
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace taskagıtmakas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        int kullanici = 0;
+        int bot = 0;
+        int tasKagitMakas;
+
+        Random random = new Random();
+
+        private void Fonksiyon()
+        {
+
+            tasKagitMakas = random.Next(0, 3);
+
+            if (tasKagitMakas == 0)
+            {
+                textBox4.Text = "Taş";
+            }
+            if (tasKagitMakas == 1)
+            {
+                textBox4.Text = "Kağıt";
+            }
+            if (tasKagitMakas == 2)
+            {
+                textBox4.Text = "Makas";
+            }
+            // 0 taş
+            // 1 kağıt
+            // 2 makas
+        }
+        private void allClear()
+        {
+            textBox1.Clear();
+            textBox4.Clear();
+
+            label1.Text = "Bot : 0";
+            label2.Text = "Kullanıcı : 0";
+
+            kullanici = 0;
+            bot = 0;
+        }
+        private void Skor()
+        {
+            if (kullanici == 3)
+            {
+                MessageBox.Show("Oyunu Kazandınız!");
+                allClear();
+            }
+            if (bot == 3)
+            {
+                MessageBox.Show("Oyunu Kaybettiniz!");
+                allClear();
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Fonksiyon();
+            if (tasKagitMakas == 0){textBox1.Text = "Berabere!";}
+            else if (tasKagitMakas == 1)
+            {
+                textBox1.Text = "Kaybettiniz!";
+                bot += 1;
+                label1.Text = "Bot : " + bot.ToString();
+            }
+            else
+            {
+                textBox1.Text = "Kazandınız!";
+                kullanici += 1;
+                label2.Text = "Kullanıcı : " + kullanici.ToString();
+            }
+            Skor();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Fonksiyon();
+            if (tasKagitMakas == 0)
+            {
+                textBox1.Text = "Kazandınız!";
+                kullanici += 1;
+                label2.Text = "Kullanıcı : " + kullanici.ToString();
+            }
+            else if (tasKagitMakas == 1){textBox1.Text = "Berabere!";}
+            else
+            {
+                textBox1.Text = "Kaybettiniz!";
+                bot += 1;
+                label1.Text = "Bot : " + bot.ToString();
+            }
+            Skor();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Fonksiyon();
+            if (tasKagitMakas == 0)
+            {
+                textBox1.Text = "Kaybettiniz!";
+                bot += 1;
+                label1.Text = "Bot : " + bot.ToString();
+            }
+            else if (tasKagitMakas == 1)
+            {
+                textBox1.Text = "Kazandınız!";
+                kullanici += 1;
+                label2.Text = "Kullanıcı : " + kullanici.ToString();
+            }
+            else{textBox1.Text = "Berabere!";}
+            Skor();
+        }
+    }
+}
